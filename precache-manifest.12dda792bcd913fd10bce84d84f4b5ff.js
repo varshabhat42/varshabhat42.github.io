69d118e5b2a7119f3577b189e1e087d9dec8eb6f
Varsha Bhat <varshava@usc.edu>
2025-10-02 23:24:25 -0700

    Deploy portfolio website

---
+++ precache-manifest.12dda792bcd913fd10bce84d84f4b5ff.js
@@ -0,0 +1,278 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "178ca6326bf8f25613e23a0d25177dbb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "72a9ebcf70aa4a711d6b",
+    "url": "./static/css/main.de451233.chunk.css"
+  },
+  {
+    "revision": "f5fee27f06f71735c30d",
+    "url": "./static/js/2.36cec9e7.chunk.js"
+  },
+  {
+    "revision": "c21d579849576bf6a47550572ff70f62",
+    "url": "./static/js/2.36cec9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a9ebcf70aa4a711d6b",
+    "url": "./static/js/main.5595aa3a.chunk.js"
+  },
+  {
+    "revision": "b5b95e76b5f115bc9c19",
+    "url": "./static/js/runtime-main.131c30dd.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "./static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "aabb16f4d09a1c0590d7613cc0d2de0b",
+    "url": "./static/media/CloudInfraIcon.aabb16f4.png"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "./static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "./static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "./static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "./static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "./static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "./static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "35f049fbbf5a9edfa4d53a7514274e72",
+    "url": "./static/media/LadyDeveloperIcon.35f049fb.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "./static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "163f3224ebb02ccd9e102bceaff73c18",
+    "url": "./static/media/PortfolioMainIcon.163f3224.png"
+  },
+  {
+    "revision": "b3ae557d1553c5dc4ea6a6c6f0e96955",
+    "url": "./static/media/Varsha-profile-pic-cropped.b3ae557d.jpg"
+  },
+  {
+    "revision": "3f2eaf83219f1030a5c02cace5e09516",
+    "url": "./static/media/Woman-experience-1.3f2eaf83.png"
+  },
+  {
+    "revision": "074c759bcddffa259a562604f23085a0",
+    "url": "./static/media/bauddhikgeeks-transparent.074c759b.png"
+  },
+  {
+    "revision": "f2d490dc98d16b9d7939e8006d0debb7",
+    "url": "./static/media/bauddhikgeeks.f2d490dc.png"
+  },
+  {
+    "revision": "a1051a228140c97862ba9a7f321c92b6",
+    "url": "./static/media/buld.a1051a22.jpg"
+  },
+  {
+    "revision": "ec8e1b3c813a5cc0939965b7c56a03db",
+    "url": "./static/media/crest-1.ec8e1b3c.png"
+  },
+  {
+    "revision": "404ae7bc061a03e399daa61344c78566",
+    "url": "./static/media/cwoc.404ae7bc.png"
+  },
+  {
+    "revision": "82473004d117a65c8ed30396c6299763",
+    "url": "./static/media/efi.82473004.png"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "./static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "./static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "./static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "./static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "./static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "./static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "./static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "./static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "./static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "./static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "./static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "./static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "./static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "./static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "./static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "54f6721948491acb1997fd43496cd7df",
+    "url": "./static/media/flutter.54f67219.png"
+  },
+  {
+    "revision": "6364dbb3a5b59c28566eac7e6aaacd04",
+    "url": "./static/media/gdg.6364dbb3.png"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "./static/media/github.0ba2aa20.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "./static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "3d9465c3483dffb591b31f101699d0dc",
+    "url": "./static/media/gtu.3d9465c3.png"
+  },
+  {
+    "revision": "7a2dbf7a2595600c00118eea8a7a52f8",
+    "url": "./static/media/hackathon1.7a2dbf7a.svg"
+  },
+  {
+    "revision": "85891c5d1be619f9e578944e0504fe7e",
+    "url": "./static/media/harikrushn.85891c5d.jpg"
+  },
+  {
+    "revision": "207bd193c8484a100c1e0a52b0c58fdf",
+    "url": "./static/media/iisc.207bd193.png"
+  },
+  {
+    "revision": "956a66cf6e6cdc3c8261a9e467e7497f",
+    "url": "./static/media/iisc.956a66cf.jpg"
+  },
+  {
+    "revision": "36d7942a201d6e2b83b55876e8433ccf",
+    "url": "./static/media/iit.36d7942a.png"
+  },
+  {
+    "revision": "ccf886614a9c68f209d3e95c2626dd99",
+    "url": "./static/media/ino.ccf88661.png"
+  },
+  {
+    "revision": "847873a85df8045d1d51cc6cf9cc97d7",
+    "url": "./static/media/localguide.847873a8.gif"
+  },
+  {
+    "revision": "b7f903d747b0409347c9ee67745dbfa4",
+    "url": "./static/media/localguide.b7f903d7.png"
+  },
+  {
+    "revision": "0bab33d27e97c1a6528bc67e7022003f",
+    "url": "./static/media/lumen.0bab33d2.jpg"
+  },
+  {
+    "revision": "bc5cfd3a887013976863247e66c389a2",
+    "url": "./static/media/mlh-logo.bc5cfd3a.svg"
+  },
+  {
+    "revision": "53dd27a8801e3a4216b9c9e217dfc7d0",
+    "url": "./static/media/mlh.53dd27a8.png"
+  },
+  {
+    "revision": "d324ee17433678f2c32e57198b98c6db",
+    "url": "./static/media/mongo.d324ee17.png"
+  },
+  {
+    "revision": "4b7baf41595be34189ac7ea238ed9545",
+    "url": "./static/media/msrit.4b7baf41.png"
+  },
+  {
+    "revision": "5dcd18d03068fbd9edeeb5003b21998a",
+    "url": "./static/media/nightowls.5dcd18d0.jpg"
+  },
+  {
+    "revision": "22f11067f2d745aaceb51e485720635d",
+    "url": "./static/media/postman.22f11067.jpg"
+  },
+  {
+    "revision": "c108f55777ceaa6690606a207b737d0a",
+    "url": "./static/media/postman.c108f557.png"
+  },
+  {
+    "revision": "759b2ef3cd0650485f91a611e5e10f15",
+    "url": "./static/media/projects_image_girl.759b2ef3.png"
+  },
+  {
+    "revision": "4b5650d6929850b29fc60a2304e5f635",
+    "url": "./static/media/projects_image_girl_2.4b5650d6.png"
+  },
+  {
+    "revision": "7b7a76ef55658f6999ce00c5d55f85a5",
+    "url": "./static/media/skillenza.7b7a76ef.png"
+  },
+  {
+    "revision": "8901092d61bfd611f75368233b1774c6",
+    "url": "./static/media/ssec.8901092d.png"
+  },
+  {
+    "revision": "894ef0fc8dfe1c0ab658636b806049db",
+    "url": "./static/media/ssgandhy.894ef0fc.png"
+  },
+  {
+    "revision": "2d3b8b5d378b3a36191f3f8a5a177189",
+    "url": "./static/media/uscLogo.2d3b8b5d.png"
+  },
+  {
+    "revision": "246ad3ede6a55b0102afa9871cfcbf21",
+    "url": "./static/media/valora.246ad3ed.jpg"
+  },
+  {
+    "revision": "02fdae4350ca45cabdc9c40cc11efc2a",
+    "url": "./static/media/wrighter.02fdae43.jpg"
+  }
+]);
